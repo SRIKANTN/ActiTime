096c8b5bd958ceb83110c7173d6c8c61098e75e5
VinayN123 <vinayn348@gmail.com>
2019-07-29 15:35:39 +0530

    2nd cng

---
+++ src/qsp/demo.java
@@ -0,0 +1,14 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo 
+{
+	@Test
+	public void testA()
+	{
+		Reporter.log("selenium code",true);
+	}
+
+}
